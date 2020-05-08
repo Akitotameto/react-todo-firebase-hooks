b4846595b7b590b6d86d328153b5e6f10af852ed
Akitotameto <akito.tameto@gmail.com>
2020-05-08 11:41:48 +0900

    Firebaseへの登録処理実装

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,11 +1,52 @@
 import React, { useState } from 'react'
+import { db } from '../index'
+import firebase from 'firebase';
+import { prependOnceListener } from 'cluster';
+
+type TodoDataType = {
+    id: number,
+    title: string,
+    content: string,
+    createdAt: string,
+    category: number,
+    isFavorite: boolean
+}
 
 export const Form: React.FC = () => {
     const [title, setTitle] = useState<string>('')
     const [content, setContent] = useState<string>('')
     const [category, setCategory] = useState<number>(0)
-    const send  = () => {
-        console.log('テスト', title, content, category)
+
+    // ドキュメント数
+    // let todoData : TodoDataType[] = []
+    //         const datas = db.collection('todo').get().then(d => {
+    //             d.forEach(doc => {
+    //                 todoData.push({
+    //                     id: doc.data().id,
+    //                     title: doc.data().title,
+    //                     content: doc.data().content,
+    //                     createdAt: doc.data().createdAt,
+    //                     category: doc.data().category,
+    //                     isFavorite: doc.data().isFavorite
+    //                 })
+    //             })
+    //         })
+    // console.log('カウント数', todoData.length)
+
+    const onSubmit = async () => {
+        // 登録した日付を取得
+        // let createdAt = firebase.firestore.FieldValue.serverTimestamp()
+
+        // 登録処理
+        const adddata = db.collection('todo').doc()
+        await adddata.set({
+        id: 0,
+        title: title,
+        content: content,
+        category: category,
+        createdAt: '2020-05-08',
+        isFavorite: false
+        })
     }
 
     return(
